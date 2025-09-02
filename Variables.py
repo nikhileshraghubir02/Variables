52eca004700dc49c6ddafe74ab0595074e1d07c5
Nikhilesh Raghubir <nikhilesh.raghubir02@gmail.com>
2025-09-02 19:17:07 -0400

    Initial Commit

---
+++ Variables.py
@@ -0,0 +1,27 @@
+number1 = int(input("Enter first number for multiplication: "))
+number2 = int(input("Enter second number for multiplication: "))
+Product = number1 * number2
+print("The answer to your query is:", Product)
+
+number3 = int(input("Enter numerator for division: "))
+number4 = int(input("Enter denominator for division: "))
+Division = number3 / number4
+print("The answer to your query is:", Division)
+
+DivisionWithoutRemainder = number3 // number4
+print("The answer to your query is:", DivisionWithoutRemainder)
+
+DivisionWithRemainder = number3 % number4
+print("The answer to your query is:", DivisionWithRemainder)
+
+number5 = int(input("Enter first number to add: "))
+number6 = int(input("Enter second number to add: "))
+Sum = number5 + number6
+print("The answer to your query is:", Sum)
+
+number7 = int(input("Enter first number to subtract: "))
+number8 = int(input("Enter second number to subtract: "))
+Difference = number7 - number8
+print("The answer to your query is:", Difference)
+
+print("My name is Nikhilesh Raghubir")
